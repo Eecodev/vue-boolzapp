471816dcfa4c8c7b92352c1cee357c0e2d93142e
Eecodev <mnasser.bbk@gmail.com>
2023-11-05 09:27:43 +0100

    milestone 2 and 3 completed

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ createApp({
             contacts: contactList,
             activeContactIndex: 0,
             searchText: '',
-            message: ''
+            message: '',
+            resp: 'ok'
         }
     },
     methods:{
@@ -17,11 +18,54 @@ createApp({
             if(index !== -1){
                 this.activeContactIndex = index;
             }
+        },
+        getLastMessage(id){
+            const contact = this.contacts.find((contact)=> contact.id === id);
+            const len = contact.messages.length;
+            if(len > 0){
+                return contact.messages[len - 1].message;
+            } else{
+                return 'Non ci sono messaggi';
+            }
+        },
+        getLastAccess(id){
+            const contact = this.contacts.find((contact)=> contact.id === id);
+            const len = contact.messages.length;
+            if(len > 0){
+                return contact.messages[len -1].date;
+            } else{
+                return 'Unknown';
+            }
+        },
+        newMessage(){
+            const newMsg = {
+                date:new Date(),
+                message: this.message,
+                status: 'sent'
+            };
+            this.activeContact.messages.push(newMsg);
+            this.message = '';
+            setTimeout(()=>{
+                const newMsg = {
+                    date: new Date(),
+                    message: this.resp,
+                    status: 'received'
+                };
+                this.activeContact.messages.push(newMsg);
+            }, 1000)
         }
     },
     computed:{
         activeContact(){
             return this.contacts[this.activeContactIndex];
+        },
+        lastDate(){
+            const len = this.activeContact.messages.length;
+            if(len > 0){
+                return this.activeContact.messages[len - 1].date;
+            } else{
+                return 'Unknown';
+            }
         }
     }
 }).mount('#app');
